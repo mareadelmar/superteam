bec0422dedeaa4d051a7f86bd6316bf4987a7b9c
mareadelmar <mar.ortega.v@gmail.com>
2021-11-02 12:27:35 -0300

    Fix - keep session open

--- src/context/UserDataContext.js
+++ src/context/UserDataContext.js
@@ -3,32 +3,32 @@ import React, { useState } from "react";
 const UserContext = React.createContext({});
 
 export function UserDataContext({ children }) {
-    const initialTokenValue = null;
-    const [token, setToken] = useState(() => {
-        try {
-            const item = window.localStorage.getItem("token");
-            return item ? item : initialTokenValue;
-        } catch (error) {
-            console.error(error);
-        }
-    });
-    const [teamGood, setTeamGood] = useState([]);
-    const [teamBad, setTeamBad] = useState([]);
+	const initialTokenValue = true; // KEEP SESSION OPEN
+	const [token, setToken] = useState(() => {
+		try {
+			const item = window.localStorage.getItem("token");
+			return item ? item : initialTokenValue;
+		} catch (error) {
+			console.error(error);
+		}
+	});
+	const [teamGood, setTeamGood] = useState([]);
+	const [teamBad, setTeamBad] = useState([]);
 
-    return (
-        <UserContext.Provider
-            value={{
-                token,
-                teamGood,
-                teamBad,
-                setToken,
-                setTeamGood,
-                setTeamBad,
-            }}
-        >
-            {children}
-        </UserContext.Provider>
-    );
+	return (
+		<UserContext.Provider
+			value={{
+				token,
+				teamGood,
+				teamBad,
+				setToken,
+				setTeamGood,
+				setTeamBad,
+			}}
+		>
+			{children}
+		</UserContext.Provider>
+	);
 }
 
 export default UserContext;
